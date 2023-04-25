48c8179ae60ba009e130f4ed6209e9080e1ef9f6
Basimov Rustam <sombra1nf@yandex.ru>
2023-04-25 17:52:51 +0500

    Create project

---
+++ singlePage/src/main/java/ru/BashTeam/singlePage/SinglePageSagsApplication.java
@@ -0,0 +1,13 @@
+package ru.BashTeam.singlePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglePageSagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglePageSagsApplication.class, args);
+	}
+
+}
---
+++ singlePage/src/test/java/ru/BashTeam/singlePage/SinglePageSagsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.BashTeam.singlePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglePageSagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
